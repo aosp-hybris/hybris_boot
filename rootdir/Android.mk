fb79f63e48f1e67fc5c9de2f3168bb8921ee8b6f
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-05-20 10:08:21 +0800

    use dirty way to fix copy file error
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,40 +4,13 @@ include $(CLEAR_VARS)
 include $(LOCAL_PATH)/../hybris_boot.mk
 
 # files that live under /...
-
-file := $(TARGET_HYBRIS_BOOT_OUT)/init.rc
-$(file) : $(LOCAL_PATH)/init.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-ALL_PREBUILT += $(file)
-$(INSTALLED_RAMDISK_TARGET): $(file)
-
-file := $(TARGET_HYBRIS_BOOT_OUT)/ueventd.rc
-$(file) : $(LOCAL_PATH)/ueventd.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-ALL_PREBUILT += $(file)
-$(INSTALLED_RAMDISK_TARGET): $(file)
-
-#
-
+$(shell cp -rf $(LOCAL_PATH)/init.rc $(TARGET_HYBRIS_BOOT_OUT))
 $(shell cp -rf $(LOCAL_PATH)/init.usb.rc $(TARGET_HYBRIS_BOOT_OUT))
-
-file := $(TARGET_HYBRIS_BOOT_OUT)/ueventd.goldfish.rc
-$(file) : $(LOCAL_PATH)/ueventd.goldfish.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-ALL_PREBUILT += $(file)
-$(INSTALLED_RAMDISK_TARGET): $(file)
+$(shell cp -rf $(LOCAL_PATH)/ueventd.rc $(TARGET_HYBRIS_BOOT_OUT))
+$(shell cp -rf $(LOCAL_PATH)/ueventd.goldfish.rc $(TARGET_HYBRIS_BOOT_OUT))
 
 # create some directories (some are mount points)
-DIRS := $(addprefix $(TARGET_HYBRIS_BOOT_OUT)/, \
-		sbin \
-		dev \
-		proc \
-		sys \
-	) \
-	$(TARGET_OUT_DATA)
-
-$(DIRS):
-	@echo Directory: $@
-	@mkdir -p $@
-
-ALL_PREBUILT += $(DIRS)
+$(shell mkdir -p $(TARGET_HYBRIS_BOOT_OUT)/sbin)
+$(shell mkdir -p $(TARGET_HYBRIS_BOOT_OUT)/dev)
+$(shell mkdir -p $(TARGET_HYBRIS_BOOT_OUT)/proc)
+$(shell mkdir -p $(TARGET_HYBRIS_BOOT_OUT)/sys)
